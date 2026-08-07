17d2086492810c60262f03cf7b321658a25db979
haipholo2k3 <hainbph53579@gmail.com>
2026-08-07 16:07:28 +0700

    set up already

--- database/mongo.go
+++ database/mongo.go
@@ -1,5 +1,6 @@
 package database
-import(
+
+import (
 	"context"
 	"log"
 	"time"
@@ -8,3 +9,19 @@ import(
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+func Connect(uri string) (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, err
+	}
+
+	log.Println("Connected to MongoDB")
+	return client, nil
+}
